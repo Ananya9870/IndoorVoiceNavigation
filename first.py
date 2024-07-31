48c403f93f0a8ff7f5be8ea21104368640d99428
Suhani Sinha <suhanisinha1623@gmail.com>
2024-08-01 00:05:54 +0530

    speech rec and action

--- first.py
+++ first.py
@@ -1,6 +1,7 @@
+#pip install setuptools, SpeechRecognition, pyaudio, pyttsx3, text-to-speech
 #Speech Recognition Part
 import speech_recognition as sr
-
+import pyttsx3
 # Initialize the recognizer
 recognizer = sr.Recognizer()
 
@@ -17,3 +18,33 @@ with sr.Microphone() as source:
         print("Sorry, I could not understand the audio.")
     except sr.RequestError:
         print("Could not request results from Google Speech Recognition service.")
+
+engine=pyttsx3.init()
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+import webbrowser
+ltext=text.lower()
+if"open google" in ltext:
+    webbrowser.open("https://google.com/")
+    response ="opening Google"
+    print(response)
+    speak(response)
+    
+
+elif"open youtube" in ltext:
+    webbrowser.open("https://www.youtube.com/")
+    print("Opening youtube")
+    response ="opening Youtube"
+    print(response)
+    speak(response)
+
+elif"play music" in ltext:
+    webbrowser.open("https://www.youtube.com/results?search_query=music")
+    response ="opening music on Youtube"
+    print(response)
+    speak(response)
+
+
+
