c7d327a69e8208a39fd7d4398ae6bba333267b57
Vaibhav Poddar <vaibhavpoddar09@gmail.com>
2024-08-03 11:46:19 +0530

    Changed llm.ipynb & will be working on google colab

--- llm.py
+++ llm.py
@@ -1,11 +1,3 @@
-import os
-from langchain_groq import ChatGroq
+import webbrowser
 
-from dotenv import load_dotenv
-load_dotenv()
-
-## load the Groq API key
-groq_api_key=os.environ['GROQ_API_KEY']
-
-llm=ChatGroq(model="mixtral-8x7b-32768",temperature=0.0)
-prompt="Can you provide me the code for the following comand using os:"
+webbrowser.open("https://www.google.com/search?q=dog")
